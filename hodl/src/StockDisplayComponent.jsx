97dd105feaa76c8ee1267d56a0bee11219375dbb
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-04-08 23:50:52 +0800

    Update StockDisplayComponent.jsx

--- hodl/src/StockDisplayComponent.jsx
+++ hodl/src/StockDisplayComponent.jsx
@@ -1,21 +1,45 @@
 import { useStockContext } from "./contexts/StockContext"
+import { useCallback } from "react"
 
 const StockDisplayComponent = () => {
 
   const {stocks} = useStockContext()
 
-  console.log(stocks)
+  const getStockPrice = useCallback(async (ticker) => {
+
+    try {
+
+      const res = await fetch(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo`
+      )
+      const data = await res.json()
+      const price = data['Global Quote']?.['05. price']
+
+      // console.log("Price:", price)
+
+      return price
+
+    } catch (err) {
+
+      console.error("Error fetching stock price:", err)
+      return 0
+
+    }
+
+  }, []);
 
   return (
     <>
-      {stocks?.map((item) => (
-        <div>
-          <p>{item.ticker}</p>
+      {stocks?.map((item, index) => (
+        <div key={index}>
+          <b>Symbol: {item.ticker}</b>
+          <p>Quantity: {item.quantity}</p>
+          <p>Purchase price: {item.price}</p>
+          <p>Current price: {getStockPrice(item.ticker)}</p>
         </div>
       ))}
     </>
   )
-
 }
 
 export default StockDisplayComponent
