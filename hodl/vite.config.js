dd5e3f872ea65d03d5c461ab5ecf3f1e0c5ec3c1
servicebulletin <36870168+servicebulletin@users.noreply.github.com>
2025-04-10 21:01:11 +0800

    Update vite.config.js

--- hodl/vite.config.js
+++ hodl/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/se100-capstone/',
+  base: '/se100-capstone/hodl/',
 })
